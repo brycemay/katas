dec83266cbfb7d2cf3a1ad4a4343153e711dfdbf
Bryce <bryce.may.8@hotmail.com>
2019-04-15 11:30:45 -0600

    second lesson

--- kata01.js
+++ kata01.js
@@ -5,6 +5,7 @@
 describe('A template string, is wrapped in ` (backticks) instead of \' or "', function() {
     describe('by default, behaves like a normal string', function() {
       it('just surrounded by backticks', function() {
+        //var str = ``;
         var str = `like a string`; //made str into a string
         assert.equal(str, 'like a string');
       });
@@ -19,6 +20,7 @@ describe('A template string, is wrapped in ` (backticks) instead of \' or "', fu
         assert.equal(evaluated, 'x=' + x);
       });
       it('multiple variables get evaluated too', function() {
+        //var evaluated = `${ x } + ${ y }`;
         var evaluated = `${x}+${y}`; //removed the spaces between x and y curly brackets, 
         assert.equal(evaluated, x + '+' + y); //then added backticks
       });
@@ -44,4 +46,38 @@ describe('A template string, is wrapped in ` (backticks) instead of \' or "', fu
     });
   });
   
-  
+ // 2: template strings - multiline
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('Template string, can contain multiline content', function() {
+  it('wrap it in backticks (`) and add a newline, to span across two lines', function() {
+    //var normalString = `line1 //// line3`;
+  var normalString = `line1\n\nline3`;
+    assert.equal(normalString, `line1\n\nline3`);
+  });
+  it('even over more than two lines', function() {
+    //var multiline = ``;
+    var multiline = `\n\n\n`;//added '\n\n\n'
+	    
+	  
+    assert.equal(multiline.split('\n').length, 4);
+  });
+  describe('and expressions inside work too', function() {
+    var x = 42;
+    it('like simple variables', function() {
+      //var multiline = `line 1 $ {x}`;
+      var multiline = `line 1\n
+      ${x}`;//added a \n after "line" then hit enter
+
+      
+      assert.equal(multiline, 'line 1\n\n      42');
+    });
+    it('also here spaces matter', function() {
+      //var multiline = ``;
+      var multiline = `\n\n42`;
+
+      assert.equal(multiline, '\n\n42');
+    });
+  });
+});
