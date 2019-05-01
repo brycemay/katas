9009e36f5c32f41ca7b5fb3f01cc2593ac1cf9e6
Bryce <bryce.may.8@hotmail.com>
2019-05-01 09:41:42 -0600

    added kata 13

--- kata01.js
+++ kata01.js
@@ -465,3 +465,37 @@ describe('Destructure objects', () => {
     });
   });
 });
+
+
+// 13: destructuring - defaults
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('When destructuring you can also provide default values', () => {
+  it('just assign a default value, like so `a=1`', () => {
+  //const [a:1] = [];
+    const [a=1] = []; //assigned 'a' a default value
+    assert.equal(a, 1);
+  });
+  it('for a missing value', () => {
+  //const [b=2] = [1,,3];
+    const [a,b=2,c] = [1,,3];
+    assert.equal(b, 2);
+  });
+  it('in an object', () => {
+  //const [a, b=2] = {a: 1};
+    const {a, b=2} = {a: 1};// 
+    assert.equal(b, 2);
+  });
+  it('if the value is undefined', () => {
+  //const {a, b} = {a: 1, b: void 0};
+    const {a, b=2} = {a: 1, b: void 0};//assigning b to 2
+    assert.strictEqual(b, 2);
+  });
+  it('also a string works with defaults', () => {
+  //const [b=2] = '1';
+    const [a, b=2] = '1';
+    assert.equal(a, '1');
+    assert.equal(b, 2);
+  });
+});
