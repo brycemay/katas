57ff0740581cde68c7b7468598b13774a762719e
Bryce <bryce.may.8@hotmail.com>
2019-05-06 10:17:07 -0600

    added somne katas

--- kata01.js
+++ kata01.js
@@ -659,3 +659,67 @@ describe('Unicode in strings', () => {
     assert.equal(nuclear, 'no more ☢');
   });
 });
+
+
+// 19: rest - with-destructuring
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Rest parameters with destructuring', () => {
+  it('must be last', () => {
+  //const [...all, last] = [1, 2, 3, 4];
+    const [...all] = [1, 2, 3, 4]; //
+    assert.deepEqual(all, [1, 2, 3, 4]);
+  });
+  it('assign rest of an array to a variable', () => {
+  //const [...all] = [1, 2, 3, 4];
+    const [x,...all] = [1, 2, 3, 4];// x skips the first one. rest calls whats left over
+    assert.deepEqual(all, [2, 3, 4]);
+  });
+  // the following are actually using `spread` ... oops, to be fixed #TODO
+  it('concat differently', () => {
+    const theEnd = [3, 4];
+  //const allInOne = [1, 2, ...[theEnd]];
+    const allInOne = [1, 2, ...theEnd];
+    assert.deepEqual(allInOne, [1, 2, 3, 4]);
+  });
+  it('`apply` made simple, even for constructors', () => {
+    const theDate = [2015, 1, 1];
+  //const date = new Date(theDate);
+    const date = new Date(...theDate);//??
+    assert.deepEqual(new Date(2015, 1, 1), date);
+  });
+});
+
+// 21: spread - with-strings
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Spread syntax with strings', () => {
+  it('expands each character of a string by prefixing it with `...`', function() {
+  //const [b, a] = [...'ab'];
+    const [a, b] = [...'ab'];
+    assert.equal(a, 'a');
+    assert.equal(b, 'b');
+  });
+  it('expands any kind of character', function() {
+  //const arr = [...'12'];
+    const arr = [...'1 ☢ 2'];
+    assert.deepEqual(arr, ['1', ' ', '☢', ' ', '2']);
+  });
+  it('works anywhere inside an array (must not be last)', function() {
+  //const letters = ['a', 'bcd', 'e', 'f'];
+    const letters = ['a', ...'bcd', 'e', 'f'];
+    assert.equal(letters.length, 6);
+  });
+  it('don`t confuse with the rest operator', function() {
+  //const [...rest] = ['1234', ...'5'];
+    const [...rest] = [...'12345'];
+    assert.deepEqual(rest, [1, 2, 3, 4, 5]);
+  });
+  it('can also be used as function parameter', function() {
+  //const max = Math.max('12345');
+    const max = Math.max(...'12345');
+    assert.deepEqual(max, 5);
+  });
+});
